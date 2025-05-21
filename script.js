4b3ab38b6e06724cd14063c50947d9e2b5fbb7ae
Taiyo <tfYoshida12@gmail.com>
2025-05-22 01:37:37 +0300

    Add interactive skill descriptions

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const toggleButton = document.getElementById("darkModeToggle");
 toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
+
+const skillButtons = document.querySelectorAll(".skill-btn");
+
+skillButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const detail = button.nextElementSibling;
+        detail.classList.toggle("hidden");
+    })
+})
